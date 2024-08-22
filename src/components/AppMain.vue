d68bc5817f1d6d08b17562671532282f0676dfe5
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-08-22 15:48:30 +0200

    Aggiunta chiamata api, aggiuto card per visualizzazione dei progetti con dati

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,32 +1,14 @@
 <script>
-    import axios from 'axios';
+    import MainCardProject from './MainCardProject.vue';
+
     export default{
         data(){
             return{
-                projects:[],
-            }
-        },
-        methods:{
-            getProjects(){
-                axios.get('/projects', {
-                    params: {
 
-                    }
-                })
-                .then((response) => {
-                    console.log(response.data.results.data);
-                    this.projects = response.data.results.data;
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-                .finally(function () {
-                    // always executed
-                });  
             }
         },
-        created(){
-            this.getProjects();
+        components:{
+            MainCardProject,
         }
     }
 </script>
@@ -34,6 +16,7 @@
 <template>
     <main>
         <h1>MAIN</h1>
+    <MainCardProject/>
     </main>
 </template>
 
---
+++ src/components/MainCardProject.vue
@@ -0,0 +1,47 @@
+<script>
+    import axios from 'axios';
+    export default{
+        data(){
+            return{
+                projects:[],
+            }
+        },
+        methods:{
+            getProjects(){
+                axios.get('http://127.0.0.1:8000/api/projects', {
+                    params: {
+
+                    }
+                })
+                .then((response) => {
+                    console.log(response.data.data);
+                    this.projects = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });  
+            }
+        },
+        created(){
+            this.getProjects();
+        }
+    }
+</script>
+
+<template>
+    <section>
+        <div class="card m-3" style="width: 18rem;" v-for="project in projects">
+            <div class="card-body">
+                <h5 class="card-title"> {{ project.name }} </h5>
+                <p class="card-text"> {{ project.description }} </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
